f592386ede85eafba90a84a8dca06eecd8fe8447
ruuuh20 <pkim2320@gmail.com>
2019-03-23 19:33:45 -0400

    removing req dotenv

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-require('dotenv').config()
+// require('dotenv').config()
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
